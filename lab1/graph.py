4a22dfb36b5d7db5e385ac5ffc05e5ace9533469
et2439 <143292003+et2439@users.noreply.github.com>
2024-08-27 18:28:12 +0300

    Update graph.py

--- lab1/graph.py
+++ lab1/graph.py
@@ -1,19 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Загрузите данные из файла Excel
 file_path = 'монетки.xlsx'
 data = pd.read_excel(file_path)
 
-# Создайте график зависимости
 plt.figure(figsize=(10, 6))
 plt.scatter(data['год чеканки'], data['масса'], color='blue', alpha=0.7)
 
-# Добавьте метки осей и заголовок
 plt.xlabel('год чеканки')
 plt.ylabel('масса')
 plt.title('зависимость массы от года чеканки')
 
-# Отобразите график
 plt.savefig('graph.png')
-
